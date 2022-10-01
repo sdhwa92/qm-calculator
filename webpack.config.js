3fde7e8580875b5054168ba0f2d3059942270ff2
Daehwa Seo <sdhwa92@gmail.com>
2022-10-02 06:57:44 +1000

    update webpack config for building

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,56 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-const isProduction =
-  typeof NODE_ENV !== "undefined" && NODE_ENV === "production";
-const mode = isProduction ? "production" : "development";
-
-module.exports = {
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-  mode,
-  entry: path.resolve(__dirname, "src", "index.tsx"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env) => {
+  const isProduction = env.production;
+  const mode = isProduction ? "production" : "development";
+  console.log(mode);
+  return {
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+    mode,
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
-      // images
-      {
-        test: /\.(png|jpg|gif|svg|ico)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[path][name].[ext]",
-              context: "src",
-              outputPath: "assets/",
-            },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
           },
-        ],
-      },
-    ],
-  },
+        },
+        // images
+        {
+          test: /\.(png|jpg|gif|svg|ico)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[path][name].[ext]",
+                context: "src",
+                outputPath: "assets/",
+              },
+            },
+          ],
+        },
+      ],
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-      favicon: "./public/favicon.ico",
-      filename: "index.html",
-      manifest: "./public/manifest.json",
-    }),
-  ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+        favicon: "./public/favicon.ico",
+        filename: "index.html",
+        manifest: "./public/manifest.json",
+      }),
+    ],
+  };
 };
